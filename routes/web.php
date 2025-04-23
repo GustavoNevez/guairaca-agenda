a249bbe98c9be18a03fd3e7e2a907530ff9553d7
gustavonevez <chips_gustavo2@outlook.com>
2025-04-23 16:11:42 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+Route::get('/', function () {
+
+    return view('posts', ['posts' => Post::latest()->with(['category', 'author'])->get()]);
+});
+
+Route::get('posts/{post:slug}', function (Post $post) {
+
+
+    return view('post', [
+        'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
+
+
+Route::get('authors/{author}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
+    ]);
+});
